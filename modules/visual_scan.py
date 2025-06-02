332425178f95ce7555ad6f12f8dcc6a182977414
Tanveer Fatima <157178050+tanveer2417@users.noreply.github.com>
2025-06-02 22:01:24 +0530

    Update visual_scan.py

--- modules/visual_scan.py
+++ modules/visual_scan.py
@@ -1,1 +1,11 @@
+import requests
+from html2image import Html2Image
+
+def capture_screenshot(url):
+    try:
+        hti = Html2Image()
+        hti.screenshot(url=url, save_as='screenshot.png')
+        print("[+] Screenshot saved as screenshot.png")
+    except Exception as e:
+        print(f"[!] Error capturing screenshot: {e}")
 
