51b229d2a6ebdad0739c85ae95a2ddcf56d03ffd
Tanveer Fatima <157178050+tanveer2417@users.noreply.github.com>
2025-06-02 22:02:40 +0530

    Update find_params.py

--- modules/find_params.py
+++ modules/find_params.py
@@ -1,1 +1,14 @@
+import requests
+
+def find_parameters(url, params=['id', 'q', 'search', 'page']):
+    reflected = []
+    for p in params:
+        test = {p: "test123"}
+        try:
+            r = requests.get(url, params=test, timeout=3)
+            if "test123" in r.text:
+                reflected.append(p)
+        except:
+            continue
+    return reflected
 
