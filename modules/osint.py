5ce098233c43e9fdfbba69fc9fd8c8460ce472a3
Tanveer Fatima <157178050+tanveer2417@users.noreply.github.com>
2025-06-02 22:01:50 +0530

    Update osint.py

--- modules/osint.py
+++ modules/osint.py
@@ -1,1 +1,16 @@
+import socket
+import re
+import whois
+
+def gather_osint(domain):
+    try:
+        data = whois.whois(domain)
+        emails = set(re.findall(r'[\w\.-]+@[\w\.-]+', str(data)))
+        return {
+            "registrar": data.registrar,
+            "creation_date": str(data.creation_date),
+            "emails": list(emails)
+        }
+    except Exception as e:
+        return {"error": str(e)}
 
