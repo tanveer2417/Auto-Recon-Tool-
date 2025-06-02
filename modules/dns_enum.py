f8a5f9f30a900b514b19876ef5ed5ff4962b44bb
Tanveer Fatima <157178050+tanveer2417@users.noreply.github.com>
2025-06-02 21:59:21 +0530

    Update dns_enum.py

--- modules/dns_enum.py
+++ modules/dns_enum.py
@@ -1,1 +1,15 @@
+import dns.resolver
+
+def dns_lookup(domain):
+    records = {}
+    try:
+        records['A'] = [r.address for r in dns.resolver.resolve(domain, 'A')]
+    except: records['A'] = []
+    try:
+        records['MX'] = [r.exchange.to_text() for r in dns.resolver.resolve(domain, 'MX')]
+    except: records['MX'] = []
+    try:
+        records['NS'] = [r.to_text() for r in dns.resolver.resolve(domain, 'NS')]
+    except: records['NS'] = []
+    return records
 
