5036b57b47010d9647f9bba41ff23479c44cf043
Tanveer Fatima <157178050+tanveer2417@users.noreply.github.com>
2025-06-02 21:59:59 +0530

    Update ssl_tls_analysis.py

--- modules/ssl_tls_analysis.py
+++ modules/ssl_tls_analysis.py
@@ -1,1 +1,13 @@
+import ssl, socket
+
+def get_cert_info(domain):
+    ctx = ssl.create_default_context()
+    with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
+        s.settimeout(3)
+        try:
+            s.connect((domain, 443))
+            cert = s.getpeercert()
+            return cert
+        except Exception as e:
+            return {"error": str(e)}
 
